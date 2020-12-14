1ddf71ec6baefa826e100b3a18e6a9183705c967
Bhagi <puppyreddy1997@gmail.com>
2020-12-14 18:56:31 +0530

    [Bhagyalaxmi]ADDED:Inserted Values into the Tables

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,3 +1,4 @@
+
 create Database Employee_Payroll
 use Employee_Payroll
 CREATE TABLE Employee_Payroll (
@@ -6,4 +7,10 @@ CREATE TABLE Employee_Payroll (
     Salary Float,
     Date Date,   
 );
-select * from Employee_payroll;
+select * from Employee_Payroll;
+INSERT INTO Employee_Payroll(Name, Salary,Date)
+VALUES ('Sravani', '60000', '2019-11-13');
+INSERT INTO Employee_Payroll(Name, Salary,Date)
+VALUES ('Tejaswini', '75000', '2018-05-27');
+INSERT INTO Employee_Payroll(Name, Salary,Date)
+VALUES ('Akshay', '100000', '2018-02-13');
