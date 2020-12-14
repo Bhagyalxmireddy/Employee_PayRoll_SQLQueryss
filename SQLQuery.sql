308270e771af19702e0cdf578554289af218d37d
Bhagi <puppyreddy1997@gmail.com>
2020-12-14 18:42:48 +0530

    [Bhagyalaxmi]ADDED:Creating the EmployeePayRoll Table

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,2 +1,9 @@
 create Database Employee_Payroll
-
+use Employee_Payroll
+CREATE TABLE Employee_Payroll (
+    ID int  IDENTITY(1,1) primary key,
+    Name varchar(50),
+    Salary Float,
+    Date Date,   
+);
+select * from Employee_payroll;
